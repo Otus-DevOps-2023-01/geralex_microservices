4dd889823cc5220856ee7e9f7295507b7d44cf79
Gerasimov Aleksandr <gerasimov.a.i@cifro.tech>
2023-06-25 18:13:45 +0300

    Выполнено ДЗ по kubernetes-2

--- kubernetes/terraform/cluster_app_install.sh
+++ kubernetes/terraform/cluster_app_install.sh
@@ -4,4 +4,9 @@ set -e
 
 cd ../reddit
 
-kubectl apply -f .
+#Создадим dev namespace
+kubectl apply -f ./dev-namespace.yml
+#Задеплоим все компоненты приложения в namespace dev
+kubectl apply -f . -n dev
+#kubectl apply -f ./ui-deployment.yml -n dev
+#kubectl apply -f ./ui-service.yml -n dev
--- kubernetes/terraform/terraform/k8s-cluster.tf
+++ kubernetes/terraform/terraform/k8s-cluster.tf
@@ -30,6 +30,26 @@ variable "count_instance" {
   description = "Yandex Cloud Count Instance Node"
 }
 
+variable "cores" {
+  type = string
+  description = "Yandex Cloud Count vCPU"
+}
+
+variable "memory" {
+  type = string
+  description = "Yandex Cloud Count RAM"
+}
+
+variable "disk_size" {
+  type = string
+  description = "Yandex Cloud Count Disk Size"
+}
+
+variable "disk_type" {
+  type = string
+  description = "Yandex Cloud Type Disk"
+}
+
 # Provider
 
 terraform {
@@ -59,7 +79,7 @@ resource "yandex_kubernetes_cluster" "k8s-zonal" {
       subnet_id = yandex_vpc_subnet.k8s-subnet.id
     }
 	public_ip = true # доступность из вне
-    security_group_ids = [yandex_vpc_security_group.k8s-public-services.id]
+#    security_group_ids = [yandex_vpc_security_group.k8s-public-services.id]
   }
   service_account_id      = yandex_iam_service_account.myaccount.id
   node_service_account_id = yandex_iam_service_account.myaccount.id
@@ -98,14 +118,14 @@ resource "yandex_kubernetes_node_group" "k8s_node_group" {
     }
 
     resources {
-      cores         = 4
-      memory        = 8
+      cores         = var.cores
+      memory        = var.memory
       core_fraction = 50
     }
 
     boot_disk {
-      type = "network-hdd"
-      size = 64
+      type = var.disk_type
+      size = var.disk_size
     }
 
     scheduling_policy {
@@ -237,64 +257,63 @@ resource "yandex_resourcemanager_folder_iam_member" "viewer" {
 
 #Security Group
 
-resource "yandex_vpc_security_group" "k8s-public-services" {
-  name        = "k8s-public-services"
-  description = "Правила группы разрешают подключение к сервисам из интернета. Примените правила только для групп узлов."
-  network_id  = yandex_vpc_network.otusnet.id
-  ingress {
-    protocol          = "TCP"
-    description       = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера Managed Service for Kubernetes и сервисов балансировщика."
-    predefined_target = "loadbalancer_healthchecks"
-    from_port         = 0
-    to_port           = 65535
-  }
-  ingress {
-    protocol          = "ANY"
-    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
-    predefined_target = "self_security_group"
-    from_port         = 0
-    to_port           = 65535
-  }
-  ingress {
-    protocol          = "ANY"
-    description       = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера Managed Service for Kubernetes и сервисов."
-    v4_cidr_blocks    = concat(yandex_vpc_subnet.k8s-subnet.v4_cidr_blocks)
-    from_port         = 0
-    to_port           = 65535
-  }
-  ingress {
-    protocol          = "ICMP"
-    description       = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
-    v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-  }
-  ingress {
-    protocol          = "TCP"
-    description       = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
-    v4_cidr_blocks    = ["0.0.0.0/0"]
-    from_port         = 30000
-    to_port           = 32767
-  }
-  ingress {
-    protocol       = "TCP"
-    description    = "Правило разрешает подключение к API Kubernetes через порт 6443 из указанной сети."
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 6443
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Правило разрешает подключение к API Kubernetes через порт 443 из указанной сети."
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 443
-  }
-  egress {
-    protocol          = "ANY"
-    description       = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
-    v4_cidr_blocks    = ["0.0.0.0/0"]
-    from_port         = 0
-    to_port           = 65535
-  }
-}
+#resource "yandex_vpc_security_group" "k8s-public-services" {
+#  name        = "k8s-public-services"
+#  description = "Правила группы разрешают подключение к сервисам из интернета. Примените правила только для групп узлов."
+#  network_id  = yandex_vpc_network.otusnet.id
+#  ingress {
+#    protocol          = "TCP"
+#    description       = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера Managed Service for Kubernetes и сервисов балансировщика."
+#    predefined_target = "loadbalancer_healthchecks"
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#  ingress {
+#    protocol          = "ANY"
+#    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
+#    predefined_target = "self_security_group"
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#  ingress {
+#    protocol          = "ANY"
+#    description       = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера Managed Service for Kubernetes и сервисов."
+#    v4_cidr_blocks    = concat(yandex_vpc_subnet.k8s-subnet.v4_cidr_blocks)
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#  ingress {
+#    protocol          = "ICMP"
+#    description       = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
+#    v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+#  }
+#  ingress {
+#    protocol          = "TCP"
+#    description       = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
+#    v4_cidr_blocks    = ["0.0.0.0/0"]
+#    from_port         = 30000
+#    to_port           = 32767
+#  }
+#  ingress {
+#    protocol       = "TCP"
+#    description    = "Правило разрешает подключение к API Kubernetes через порт 6443 из указанной сети."
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 6443
+#  }
+#  ingress {
+#    protocol       = "TCP"
+#    description    = "Правило разрешает подключение к API Kubernetes через порт 443 из указанной сети."
+#    v4_cidr_blocks = ["0.0.0.0/0"]
+#    port           = 443
+#  }
+#  egress {
+#    protocol          = "ANY"
+#    description       = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
+#    v4_cidr_blocks    = ["0.0.0.0/0"]
+#    from_port         = 0
+#    to_port           = 65535
+#  }
+#}
 
 # Output values
 
